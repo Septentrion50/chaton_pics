48a10ea5b110f8b186072b9b4b30adad4a208b66
hugomtr <hugomaitre340@gmail.com>
2021-06-07 11:58:59 +0200

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_06_07_091359) do
 
   create_table "wallets", force: :cascade do |t|
     t.float "amount"
+    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
