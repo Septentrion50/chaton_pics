a0657dafe2eb8bf831247d1633780bfc8aabbd37
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-08 12:02:14 +0200

    Bugfix base code stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -15,7 +15,7 @@ class ChargesController < ApplicationController
       customer: customer.id,
       amount: @amount,
       description: 'Rails Stripe customer',
-      currency: 'usd',
+      currency: 'usd'
     })
   
   rescue Stripe::CardError => e
