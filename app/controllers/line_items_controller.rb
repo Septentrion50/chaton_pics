2299b51ec1c80ae8f4207362e038c2878df1b9eb
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-09 18:35:05 +0200

    added destroy redirection to line-items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,6 +24,7 @@ class LineItemsController < ApplicationController
         @line_item = @order.line_items.find(params[:id])
         @line_item.destroy
         @line_items = current_order.line_items
+        redirect_to root_path
     end
 
     private
