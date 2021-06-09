a6d5a3592be66bb3a38d825d8b7214b31cfeeae5
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-09 12:22:17 +0200

    Removed old orders and added reference to line_items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_093330) do
+ActiveRecord::Schema.define(version: 2021_06_09_101149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,9 +40,11 @@ ActiveRecord::Schema.define(version: 2021_06_09_093330) do
     t.string "stripe_id"
     t.string "token"
     t.string "currency"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "wallet_id"
+    t.index ["user_id"], name: "index_charges_on_user_id"
     t.index ["wallet_id"], name: "index_charges_on_wallet_id"
   end
 
@@ -61,15 +63,17 @@ ActiveRecord::Schema.define(version: 2021_06_09_093330) do
     t.decimal "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "order_id"
+    t.bigint "kitten_id"
+    t.index ["kitten_id"], name: "index_line_items_on_kitten_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
   end
 
   create_table "orders", force: :cascade do |t|
+    t.decimal "subtotal"
+    t.decimal "total"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "wallet_id"
-    t.bigint "charge_id"
-    t.index ["charge_id"], name: "index_orders_on_charge_id"
-    t.index ["wallet_id"], name: "index_orders_on_wallet_id"
   end
 
   create_table "users", force: :cascade do |t|
