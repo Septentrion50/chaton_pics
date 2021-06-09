38fc3278d9a00b3490b19f027be2a4462d8a3131
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-09 11:37:04 +0200

    Removed Wallet model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_082930) do
+ActiveRecord::Schema.define(version: 2021_06_09_093330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,12 +84,6 @@ ActiveRecord::Schema.define(version: 2021_06_09_082930) do
     t.string "lastname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "wallets", force: :cascade do |t|
-    t.float "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
