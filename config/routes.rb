593c3b403c048c3766282ad4878fcf495dfc2bc0
cedricmesas <mesas.cedric@gmail.com>
2021-06-09 15:06:47 +0200

    static pages routes and rewritten url

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/terms'
-  get 'static_pages/privacy_policy'
   root to: 'kittens#index'
   get "/pages/:page" => "pages#show"
   devise_for :users
@@ -13,4 +9,15 @@ Rails.application.routes.draw do
   resources :charges, only: [:new, :create]
   resources :users, only: [:show, :edit , :update]
   resources :join_wallet_items_url
+
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+  # get 'static_pages/terms'
+  # get 'static_pages/privacy_policy'
+  
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/terms', to: 'static_pages#terms', via: 'get'
+  match '/privacy_policy', to: 'static_pages#privacy_policy', via: 'get'
+
 end
