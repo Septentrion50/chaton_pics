abfabbfb960ccc5d32c24146b9299527e7641c23
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-10 22:19:43 +0200

    modified mailer to attach pictures in mails - not working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,19 @@ class UserMailer < ApplicationMailer
     
     def welcome_email(user)
         @user = user 
-        @url  = 'https://chaton-pics-developement.herokuapp.com'
+        @url  = 'https://chaton-pics.herokuapp.com'
         mail(to: @user.email, subject: 'Bienvenue sur Kitten World, le monde merveilleux des chatons !') 
     end
 
     def order_confirmation(user, order)
         @user  = User.find(user)
         @order = Order.find(order)
-        @url   = 'https://chaton-pics-developement.herokuapp.com'
+        @url   = 'https://chaton-pics.herokuapp.com'
+        # @images = []
+        # attachments = @order.line_items
+        # attachments.each do |kit|
+        #     @images << Rails.application.routes.url_helpers.rails_blob_url(kit.kitten.picture)
+        # end
         mail(to: @user.email, subject: 'Ta commande a été reçue')
     end
     
