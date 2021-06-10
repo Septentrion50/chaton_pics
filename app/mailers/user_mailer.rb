05f450b716b76c4142c989de18329d9a1645c16a
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-10 19:34:35 +0200

    Changed back default from in user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'jeanmarcdardy@gmail.com'
+    default from: 'bernard.bianca@yopmail.com'
     
     def welcome_email(user)
         @user = user 
