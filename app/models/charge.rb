f1b34f96bec2bc23cebc44b49174060f351e9283
cedricmesas <mesas.cedric@gmail.com>
2021-06-10 16:39:43 +0200

    user mailer after order

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,3 +1,13 @@
 class Charge < ApplicationRecord
   belongs_to :user
+
+  after_create :order_confirm
+
+  private
+  
+  def order_confirm
+    UserMailer.order_confirmation(self.user_id, self.order_id).deliver_now
+  end
+
+
 end
