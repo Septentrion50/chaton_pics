74a35685b3e9ac4f6dfcc9854a419d93db148e29
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-10 23:26:54 +0200

    Modified default from adress

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-    default from: 'bernard.bianca@yopmail.com'
+    default from: 'cedrickittenworld@yopmail.com'
     
     def welcome_email(user)
         @user = user 
