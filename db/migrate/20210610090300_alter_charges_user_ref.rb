31f535fde61a996864c348bab7aee43d932abeff
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-10 16:47:02 +0200

    Fixed migrations

--- db/migrate/20210610090300_alter_charges_user_ref.rb
+++ db/migrate/20210610090300_alter_charges_user_ref.rb
@@ -1,7 +1,7 @@
 class AlterChargesUserRef < ActiveRecord::Migration[5.2]
   def change
-    remove_column :charges, :user_id
-    remove_column :charges, :order_id
+    # remove_column :charges, :user_id
+    # remove_column :charges, :order_id
 
     add_reference :charges, :user_id, index: true
     add_reference :charges, :order_id, index: true
