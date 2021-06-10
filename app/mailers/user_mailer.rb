c99271e27fc5894511ca0002f27bb5b836c49a86
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-10 19:58:12 +0200

    Changed host url in production config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ApplicationMailer
     
     def welcome_email(user)
         @user = user 
-        @url  = 'https://chaton-pics-developement.herokuapp.com'
+        @url  = 'https://chaton-pics.herokuapp.com'
         mail(to: @user.email, subject: 'Bienvenue sur Kitten World, le monde merveilleux des chatons !') 
     end
 
     def order_confirmation(user, order)
         @user  = User.find(user)
         @order = Order.find(order)
-        @url   = 'https://chaton-pics-developement.herokuapp.com'
+        @url   = 'https://chaton-pics.herokuapp.com'
         mail(to: @user.email, subject: 'Ta commande a été reçue')
     end
     
