777a27edcdc6e077d9829756ed840abf71dd986f
Septentrion50 <erwan56lecorre@gmail.com>
2021-06-11 10:58:49 +0200

    Modified domain in configs

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_LOGIN'],
     :password => ENV['SENDGRID_PWD'],
-    :domain => 'yopmail.com',
+    :domain => 'gmail.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
